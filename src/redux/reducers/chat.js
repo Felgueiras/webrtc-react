d3d9f90a1c865be2d6d78e67dc98987a4e258096
Felgueiras <rafaelfelgueiras1993@gmail.com>
2020-03-22 08:24:54 +0000

    removed debuggers

--- src/redux/reducers/chat.js
+++ src/redux/reducers/chat.js
@@ -8,7 +8,6 @@ export default function(state = initialState, action) {
   switch (action.type) {
     case SET_ID: {
       const { id } = action.payload;
-      debugger;
       return {
         ...state,
         myId: id
