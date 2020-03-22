937b84a7229cfc08769cc9fb70241c9759e44fed
Felgueiras <rafaelfelgueiras1993@gmail.com>
2020-03-22 10:22:23 +0000

    redux-persist

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./components/App/App";
 import * as serviceWorker from "./serviceWorker";
 
-import { Provider } from 'react-redux'
-import store from './redux/store'
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
+    <Provider store={store().store}>
+      <PersistGate loading={null} persistor={store().persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,20 @@
 import { createStore } from "redux";
 import rootReducer from "./reducers/index";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+import { persistStore, persistReducer } from 'redux-persist'
 
-export default createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const persistConfig = {
+  key: "root",
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export default () => {
+  let store = createStore(
+    persistedReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+  let persistor = persistStore(store);
+  return { store, persistor };
+};
